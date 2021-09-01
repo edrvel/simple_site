298b3a7eb26135fc4dedfc7a7d4540df2b465149
Eduard Ardashirov <eduard.ardashirov@gmail.com>
2021-09-01 17:27:35 +0500

    Первый коммит, добавление html шаблонов для сайта

---
+++ crm/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    order_dt = models.DateTimeField(auto_now=True, verbose_name='Дата')
+    order_name = models.CharField(max_length=200, verbose_name='Имя')
+    order_phone = models.CharField(max_length=200, verbose_name='Телефон')
+
+    def __str__(self):
+        return f'Заказ {self.order_id}'
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
