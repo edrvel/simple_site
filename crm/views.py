2691572017b181d59a874a360b39b1b682a4598a
Eduard Ardashirov <eduard.ardashirov@gmail.com>
2021-09-06 16:21:35 +0500

    Третий коммит, приложение "Цены"

--- crm/views.py
+++ crm/views.py
@@ -2,6 +2,8 @@ from cms.models import CmsSlider
 
 from django.shortcuts import render
 
+from price.models import PriceCard, PriceTable
+
 from .models import Order
 
 
@@ -10,8 +12,17 @@ from .models import Order
 
 def first_page(request):
     slider_list = CmsSlider.objects.all()
-    return render(request, './index.html', {'slider_list': slider_list,
-                                            })
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
+    dict_obj = {'slider_list': slider_list,
+                'pc_1': pc_1,
+                'pc_2': pc_2,
+                'pc_3': pc_3,
+                'price_table': price_table,
+                }
+    return render(request, './index.html', dict_obj)
 
 
 def thanks_page(request):
---
+++ price/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+
+class PriceCard(models.Model):
+    pc_id = models.AutoField(primary_key=True)
+    pc_value = models.CharField(max_length=200, verbose_name='Цена')
+    pc_description = models.CharField(max_length=200, verbose_name='Описание')
+
+    def __str__(self):
+        return self.pc_value
+
+    class Meta:
+        verbose_name = 'Цена'
+        verbose_name_plural = 'Цены'
+
+
+class PriceTable(models.Model):
+    pt_id = models.AutoField(primary_key=True)
+    pt_title = models.CharField(max_length=200, verbose_name='Название')
+    pt_old_price = models.CharField(max_length=200, verbose_name='Старая цена')
+    pt_new_price = models.CharField(max_length=200, verbose_name='Новая цена')
+
+    def __str__(self):
+        return self.pt_title
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
