acbbf87024c643804bc98bd4b2a62aed076188ef
Eduard Ardashirov <eduard.ardashirov@gmail.com>
2021-09-22 16:52:29 +0500

    Седьмой коммит, кастомизация админ панели

--- crm/models.py
+++ crm/models.py
@@ -15,7 +15,7 @@ class StatusCrm(models.Model):
         verbose_name_plural = 'Статусы'
 
 class Order(models.Model):
-    order_id = models.AutoField(primary_key=True)
+    order_id = models.AutoField(primary_key=True, verbose_name='Идентификатор заказа')
     order_dt = models.DateTimeField(auto_now=True, verbose_name='Дата')
     order_name = models.CharField(max_length=200, verbose_name='Имя')
     order_phone = models.CharField(max_length=200, verbose_name='Телефон')
